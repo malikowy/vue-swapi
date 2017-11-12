ecbfe5dc678d5ff443c411dc7daa25a79e5414c6
malikowy <gabriel.ignatiuk@gmail.com>
2017-11-12 21:21:28 +0100

    working fetching data

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,56 @@
 <template>
-    <!--<div id="app">-->
-    <!--<img src="./assets/logo.png">-->
-    <!--<HelloWorld/>-->
-    <!--</div>-->
     <div id="app">
-        <h1>My Blog</h1>
+    <!--   <img src="./assets/logo.png">
+    <HelloWorld/> -->
+    <!--  <h1>{{ alldata.count }}</h1>
+     <ul v-for="film in films">
+       <li>{{ film.episode_id }} || {{ film.title }} || {{ film.director }}</li>
+     </ul> -->
 
-        <table>
-            <thead>
-            <tr>
-                <th>Producer</th>
-                <th>Title</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="result in results">
-                <td>{{result.producer}}</td>
-                <td>{{result.title}}</td>
-            </tr>
-            </tbody>
-        </table>
+    <input type="text" v-model="search" placeholder="search title">
+    <label>Search title:</label>
+
+    <div class="card" v-for="film in filterFilms">
+        <h3>title: {{ film.title }} || {{ film.director }}</h3>
+    </div>
     </div>
 </template>
 
 <script>
-	import HelloWorld from './components/HelloWorld'
 	import axios from 'axios'
+	import HelloWorld from './components/HelloWorld'
 
 	export default {
 		name: 'app',
 		components: {
 			HelloWorld
 		},
-		data: function () {
+		data() {
 			return {
-				results: []
+				search: '',
+				films: [],
+				alldata: []
 			}
 		},
 		mounted() {
-			axios.get("https://swapi.co/api/films/")
-				.then(response => {this.results = response.data.results})
+			let self = this;
+			axios.get('https://swapi.co/api/films')
+				.then( function (res) {
+					self.films = res.data.results;
+					self.alldata = res.data;
+					console.log('Films: ', res.data.results);
+					console.log('Data: ', res.data);
+				})
+				.catch( function (error){
+					console.log('Error: ', error)
+				})
+		},
+		computed: {
+			filterFilms() {
+				return this.films.filter(film => {
+					return film.title.toLowerCase().includes(this.search.toLowerCase()) || film.director.toLowerCase().includes(this.search.toLowerCase())
+				})
+			}
 		}
 	}
 </script>
